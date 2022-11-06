ee2df8cd3c85d995a53a12b90af43f2c4185752c
Shadol <iniquity.spirit@gmail.com>
2022-11-06 15:10:40 +0800

    feat: Dependency Injection test

--- central.py
+++ central.py
@@ -1,51 +1,56 @@
-import random
 from flask import Flask, request, abort
 from linebot_service.core import BotApi, MessageEvent, TextMessage, StickerMessage
-
-app = Flask(__name__)
-app.config.from_object('config')
-
-# LINE 聊天機器人的基本資料
-bot = BotApi(app.config['CHANNEL_ACCESS_TOKEN'], app.config['CHANNEL_SECRET'])
-handler = bot.web_handler
+import configparser
 
 
-@app.route("/", methods=['POST'])
-def callback():
+def gateway(bot):
+    app = Flask(__name__)
 
-    signature = request.headers['X-Line-Signature']
+    # LINE 聊天機器人的基本資料
+    handler = bot.web_handler
 
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    @app.route("/", methods=['POST'])
+    def callback():
 
-    try:
-        print(body, signature)
-        handler.handle(body, signature)
+        signature = request.headers['X-Line-Signature']
 
-    except bot.exception():
-        abort(400)
+        body = request.get_data(as_text=True)
+        app.logger.info("Request body: " + body)
 
-    return 'OK'
+        try:
+            print(body, signature)
+            handler.handle(body, signature)
 
+        except bot.exception():
+            abort(400)
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_text_message(event):
+        return 'OK'
 
-    try:
-        bot.text_process(event)
-    except bot.exception():
-        abort(401)
+    @handler.add(MessageEvent, message=TextMessage)
+    def handle_text_message(event):
 
+        try:
+            bot.text_process(event)
+        except bot.exception():
+            abort(401)
 
-@handler.add(MessageEvent, message=StickerMessage)
-def handle_sticker_message(event):
+    @handler.add(MessageEvent, message=StickerMessage)
+    def handle_sticker_message(event):
 
-    package_id = event.message.package_id
-    sticker_id = event.message.sticker_id
+        package_id = event.message.package_id
+        sticker_id = event.message.sticker_id
 
-    bot.reply_sticker_message(event.reply_token, package_id, sticker_id)
+        bot.reply_sticker_message(event.reply_token, package_id, sticker_id)
+
+    return app
 
 
 if __name__ == "__main__":
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    token = config.get('line-bot', 'channel_access_token')
+    secret = config.get('line-bot', 'channel_secret')
+
+    app = gateway(BotApi(token, secret))
     app.debug = True
     app.run()
--- linebot_service/core.py
+++ linebot_service/core.py
@@ -1,4 +1,3 @@
-from typing import Dict
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage, SourceUser, SourceGroup, SourceRoom, TemplateSendMessage, ConfirmTemplate, MessageAction, ButtonsTemplate, ImageCarouselTemplate, ImageCarouselColumn, URIAction, PostbackAction, DatetimePickerAction, CameraAction, CameraRollAction, LocationAction, CarouselTemplate, CarouselColumn, PostbackEvent, StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage, ImageMessage, VideoMessage, AudioMessage, FileMessage, UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent, MemberJoinedEvent, MemberLeftEvent, FlexSendMessage, BubbleContainer, ImageComponent, BoxComponent, TextComponent, IconComponent, ButtonComponent, SeparatorComponent, QuickReply, QuickReplyButton, ImageSendMessage
